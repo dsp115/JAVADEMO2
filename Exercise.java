929e1fc1303428a3dfc89acedf311061eee36f7b
dsp115 <dpinale1@my.epcc.edu>
2026-02-16 21:17:30 +0000

    fruit exercise

---
+++ Exercise.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Demo
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("What fruit or berry would you give me?:");
+        
+        String fruit  = keyboard.nextLine();
+        
+
+        if(fruit.equals("banana"))
+        {
+            System.out.println("Thank you");
+        
+        }
+        else if(fruit.equals("apple"))
+        {
+            System.out.println("Thank you");
+        }
+        else 
+        {
+            System.out.println("error");
+        }
+    }
+}
