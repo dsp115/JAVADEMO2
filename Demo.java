d999d08f12f08c39c1b11b538a27ce52734dee3a
dsp115 <dpinale1@my.epcc.edu>
2026-02-16 20:31:00 +0000

    Secret code!!!!!!

--- Demo.java
+++ Demo.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class Demo
 {
     public static void main(String[] args)
     {
-        System.out.println("Coolio \nnew\nline\nin\nevery\nsingle\nword\n Pretty cool!");
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Coolio \nnew\nline\nin\nevery\nsingle\nword\n Pretty cool! What's the code?");
+        int code = keyboard.nextInt();
+
+        if(code == 7292)
+        {
+            System.out.println("Access granted!");
+        
+        }
+        else
+        {
+            System.out.println("Access Denied!: You are " + (7292 - code) + " away from the code");
+        }
     }
 }
