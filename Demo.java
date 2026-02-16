46fd8175c0688660894f80982a737b201a1fff07
dsp115 <dpinale1@my.epcc.edu>
2026-02-16 20:19:19 +0000

    pretty cool message

--- Demo.java
+++ Demo.java
@@ -2,6 +2,6 @@ public class Demo
 {
     public static void main(String[] args)
     {
-        System.out.println("Coolio \nnew\nline\nin\nevery\nsingle\nword");
+        System.out.println("Coolio \nnew\nline\nin\nevery\nsingle\nword\n Pretty cool!");
     }
 }
