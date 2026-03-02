10a1d1c095cadf38bb6d22f3caf2322c66e97dec
dsp115 <dpinale1@my.epcc.edu>
2026-03-02 21:28:58 +0000

    obesity

---
+++ ob.java
@@ -0,0 +1,37 @@
+public class ob {
+    public static void main(String[] args) {
+        
+        double bmi = 2.0;
+
+        if (bmi < 18.5) {
+
+    System.out.println("Underweight");
+
+}
+
+else if (bmi >= 18.5 && bmi < 25.0) {
+
+    System.out.println("Normal");
+
+}
+
+else if (bmi >= 25.0 && bmi < 30.0) {
+
+    System.out.println("Overweight");
+
+}
+
+else if (bmi >= 30.0) {
+
+    System.out.println("Obese");
+
+}
+int a = 23;
+int b = 7;
+double re = (double) a / (double) b;
+
+System.out.println(re);
+double n = ;
+
+    }
+}
